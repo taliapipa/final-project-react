7d005cdda1480bde8287ba0b3b41b46347dcd90f
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-07-14 18:47:47 +0200

    home

--- front/src/components/Pages/Character/CharacterDetail.jsx
+++ front/src/components/Pages/Character/CharacterDetail.jsx
@@ -1,4 +1,3 @@
-// CharacterDetail.jsx
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
--- front/src/components/Pages/Home/Home.jsx
+++ front/src/components/Pages/Home/Home.jsx
@@ -1,9 +1,32 @@
-const Home =()=>{
+import React, { useState } from 'react';
+import '../../../styles/Home.css'
+import dog from '../../../imgs/dog.jpg';
+import cat from '../../../imgs/gato.jpg';
+import catdog from '../../../imgs/gatoperro.jpeg';
+import perro from '../../../imgs/perro.jpeg';
+import dogdog from '../../../imgs/perroperro.jpg';
+import { FaAngleRight } from "react-icons/fa";
+
+const Home = () => {
+    const arrayImgs = [dog, cat, catdog, perro, dogdog];
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const carrusell = () => {
+    const nextIndex = (currentIndex + 1) % arrayImgs.length;
+    setCurrentIndex(nextIndex);
+};
+
     return (
-        <body>
+        <div className="div-banner">
+            <div className='carousel-div'>
+                <img src={arrayImgs[currentIndex]} alt="Carousel Image" className='carousel-img'/>
+            </div>
+            <div className='carousel-button'>
+                <button onClick={carrusell}><FaAngleRight/></button>
+            </div>
+        </div>
 
-        </body>
-    )
-}
+);
+};
 
-export default Home
+export default Home;
