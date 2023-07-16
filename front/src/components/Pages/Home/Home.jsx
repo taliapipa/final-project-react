910d6273a5c2384a7e2da033c0acf571df3badce
taliapipa <125282306+taliapipa@users.noreply.github.com>
2023-07-16 18:37:52 +0200

    home

--- front/src/components/Pages/Home/Home.jsx
+++ front/src/components/Pages/Home/Home.jsx
@@ -17,14 +17,33 @@ const Home = () => {
 };
 
     return (
-        <div className="div-banner">
-            <div className='carousel-div'>
-                <img src={arrayImgs[currentIndex]} alt="Carousel Image" className='carousel-img'/>
+        <body>
+            <div className="div-banner">
+                <div className='carousel-div'>
+                    <img src={arrayImgs[currentIndex]} alt="Carousel Image" className='carousel-img'/>
+                </div>
+                <div className='carousel-button'>
+                    <button onClick={carrusell}><FaAngleRight/></button>
+                </div>
             </div>
-            <div className='carousel-button'>
-                <button onClick={carrusell}><FaAngleRight/></button>
+            <div className='div-description'>
+                <h1 className='title'>Welcome to our website dedicated to dog lovers!</h1>
+                <p className='description'>Discover the perfect place where furry friends take the spotlight. Here, you'll find everything you need to know about the canine world and how to make life with your faithful companion an unforgettable experience.
+
+From care and training tips to the most popular breeds and surprising curiosities, our website is packed with invaluable information for dog lovers of all ages and levels of experience.
+
+Looking for a dog that fits your lifestyle? Our comprehensive breed finder will help you discover the perfect companion, taking into account your space, activity level, and personal preferences.
+
+Need to solve a specific behavior problem? Our team of canine training experts is here to offer you the best advice and practical solutions for any situation.
+
+And that's not all! Explore our gallery of adorable dog pictures, read heartwarming stories of dogs finding their forever homes, and join our community of dog lovers to share your experiences and learn from other passionate owners.
+
+On our website, dogs are more than just pets—they are family members. So come and join our virtual pack. Your doggy adventure starts here!
+
+Follow us on social media for exclusive content and exciting updates!</p>
             </div>
-        </div>
+        </body>
+
 
 );
 };
